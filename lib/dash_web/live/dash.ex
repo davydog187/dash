46b72294d7576534105905f3c331f8eecd9fe2e5
Dave Lucia <davelucianyc@gmail.com>
2022-03-23 15:31:06 -0400

    ticking-clock-playground

--- lib/dash_web/live/dash.ex
+++ lib/dash_web/live/dash.ex
@@ -1,26 +1,15 @@
 defmodule DashWeb.Dash do
   use Surface.LiveView
 
-  alias DashWeb.Components.{Agenda, CodeNav, Notes, Stock, TickingClock}
+  alias DashWeb.Components.{Agenda, CodeNav, Notes, Stock, Clock}
 
   def mount(_params, _session, socket) do
-    if connected?(socket) do
-      :timer.send_interval(:timer.seconds(1), :tick_clocks)
-    end
-
     socket =
       assign(socket,
-        clock: "America/New_York",
         items: fetch_agenda()
       )
 
     {:ok, socket}
-  end
-
-  def handle_info(:tick_clocks, socket) do
-    TickingClock.tick(socket.assigns.clock)
-
-    {:noreply, socket}
   end
 
   def render(assigns) do
@@ -28,7 +17,7 @@ defmodule DashWeb.Dash do
     <section class="p-8">
       <h1 class="text-6xl font-medium italic text-blue-400">Dash</h1>
       <div class="grid grid-cols-3 mt-4 gap-x-3 gap-y-16">
-      <TickingClock id={@clock} timezone={@clock} />
+        <Clock format={:standard} time={Time.utc_now()} />
       </div>
 
       <nav class="fixed right-10">
