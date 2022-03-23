704f6f95aa9ba49bb1d954aad21cda447d7375e2
Dave Lucia <davelucianyc@gmail.com>
2022-03-23 15:44:38 -0400

    clock-no-properties

--- lib/dash_web/components/clock.ex
+++ lib/dash_web/components/clock.ex
@@ -5,9 +5,6 @@ defmodule DashWeb.Components.Clock do
   A component that represents a wall-clock
   """
 
-  @doc "The timestamp to display on the clock"
-  prop time, :time, required: true
-
   def render(assigns) do
     ~F"""
     """
